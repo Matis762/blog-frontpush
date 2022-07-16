7233b4167818048c7701243833b113920fc4c0b2
Matis762 <matheus.liduvino762@gmail.com>
2022-07-16 16:35:42 -0300

    styling the menu.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Header from './components/Header';
 import Article from './components/Articles';
-import Posts from './components/Posts';
+
 
 function App() {
   return (
     //Essa função não retorna mais de um elemento por isso tem que ser emglobado, como se fosse uma div.
     <>
+      <Header />
       <h1>Meu blog</h1>
-      <Header/>
-      <Article/>
-      
+      <Article />
+
     </>
 
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,33 @@
+import logoSvg from '../../svg/logo.svg'
+
 function Header() {
 
+    const descLogo = 'Essa é a logo do blog'
+
+
     return (
-        <h1>Header.js </h1>
+        <>
+            <header className='flex-space-between'>
+                <div className='logo '>
+                    <img src={logoSvg} alt={descLogo} />
+
+                </div>
+
+                <div className="search">
+                    <input type='text'className='input-search' name='search' placeholder='Buscar artigos, podcast, destaques' />
+
+                </div>
+
+
+                <ul className='menu'>
+                    <li> <a href='#' className='nav-link'>Categories</a> </li>
+                    <li> <a href='#' className='nav-link'>About</a> </li>
+                    <li> <a href='#' className='nav-link'>Contact</a> </li>
+                </ul>
+
+
+            </header>
+        </>
     );
 }
 
